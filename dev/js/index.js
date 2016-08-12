b1ff76509552d51d24a028f5c67d02aa9cf9ae3b
qxm_idance <957696990@qq.com>
2016-08-12 18:05:19 +0800

    second time

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,1 +1,3 @@
-require('style!css!../style/main.css');
+// require('style!css!../style/main.css');
+import '../style/main.css';
+import './util/polyfill.js';
--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,37 @@ var isDev = ENV === 'dev';
 module.exports = function cfg (){
     var cfg = {};
     cfg.entry = {
-        app:path.join(devPath,'index.js'),
-        style:path.join(stylePath,'main.css')
+        app:path.join(devPath,'index.js')
+        // ,
+        // style:path.join(stylePath,'main.css') // 如果不在index.js 里面引入，就要在这里指定路径
     }
     cfg.out = {
         path: buildPath,//存放文件的绝对路径
         buildPath: isBuild ? '' : 'http://192.168.18.240:7000/',//网站运行时的访问路径
         filename: isBuild ? 'js/[name].[chunkhash:8].js' : '[name].bundle.js'
     }
+
+    if (isBuild) {
+        cfg.devtool = 'source-map';
+    } else {
+        cfg.devtool = 'eval-source-map';
+    }
+
+
     cfg.module = {
         loaders:[
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'stage-2']
+                },
+                exclude: /node_modules/
+            },
             { 
                 test: /\.css$/, 
-                loader: ExtractTextPlugin.extract('style', 'css'),
-                include:cfg.entry.style
+                loader:ExtractTextPlugin.extract('style', 'css')
             },
             { test: /\.png$/, loader: "url-loader?limit=100000" },
             { test: /\.jpg$/, loader: "file-loader" }
@@ -37,8 +54,13 @@ module.exports = function cfg (){
         new HtmlWebpackPlugin({
             template:'dev/index.html',
             inject:'body'
-        }) 
+        }),
+        new ExtractTextPlugin('[name].[chunkhash].css')
     ]
-    
+
+    cfg.devServer = {
+        contentBase: 'dev',
+        stats: 'minimal'
+    };
     return cfg;
 }();
