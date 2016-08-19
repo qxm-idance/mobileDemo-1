a692bb11223b9d32f6429bb5046b7f965b5b3002
qxm_idance <957696990@qq.com>
2016-08-19 10:01:54 +0800

    webpack config

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,3 +1,50 @@
 // require('style!css!../style/main.css');
 import '../style/main.css';
 import './util/polyfill.js';
+
+const status = document.getElementById('J-Status');
+const ol = document.querySelectorAll('.J-D');
+console.log(ol);
+const setStyle = (ol, len) => {
+    let docEl = document.documentElement;
+    let style = document.createElement('style');
+    docEl.firstElementChild.appendChild(style);
+    let hei = ol.offsetHeight;
+    console.log(hei);
+    ol.parentNode.style.cssText = `;-webkit-animation: marquees ${len}s linear infinite; animation: marquees ${len}s linear infinite;`
+    style.innerHTML = `@-webkit-keyframes marquees {from {-webkit-transform: translate(0, 0);  transform: translate( 0, 0); }to {  -webkit-transform: translate(0, -${hei}px); transform: translate(0, -${hei}px);} }@keyframes marquees {from {-webkit-transform: translate(0, 0);transform: translate(0, 0);}to {-webkit-transform: translate(0, -${hei}px);transform: translate(0, -${hei}px);}}`;
+};
+
+function getData(){
+	var res = [{'records':'张三：187 xxxx 7777   100'},
+	{'records':'李四：187 xxxx 9999   200'},
+	{'records':'王二：187 xxxx 6666   300'},
+	{'records':'王yi：187 xxxx 1111   300'},
+	{'records':'王er：187 xxxx 2222   300'},
+	{'records':'王sa：187 xxxx 5555   300'},
+	{'records':'王si：187 xxxx 4444   300'}]
+
+		
+	if(res && res.length>0){
+		let tmp = [];
+		res.map(it => {
+			tmp.push(`<li>${it.records}</li>`)
+		})
+
+		Array.prototype.slice.call(ol).map(it => {
+            it.innerHTML = tmp.join('');
+        });
+
+        if (res.length < 4) {
+	        ol[0].parentNode.removeChild(ol[1]);
+	    } else {
+	        setStyle(ol[0], res.length);
+	    }
+
+	}
+
+	
+}
+
+getData();
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var autoprefixer = require('autoprefixer');
+var node_modules_dir = path.join(__dirname,'node_modules');
+var deps = [
+    'react/dist/react.min.js',
+    'react-dom/dist/react-dom.min.js',
+    'react-router/dist/Router.js'];
+
+// alias配置项，可以为常用模块配置改属性，可以节省编译的搜索时间 
+//module.resolve.alias:{'react':path.join(node_modules_dir,'react/dist/react.min.js')}
+// var info = autoprefixer({ browsers: ['last 1 version'] }).info();
+// console.log(info); 这个可以打印出 浏览器的 信息
 
 var devPath = path.join(__dirname + '/dev/js');
 var buildPath = path.join(__dirname + '/build');
@@ -16,21 +29,22 @@ module.exports = function cfg (){
         app:path.join(devPath,'index.js')
         // ,
         // style:path.join(stylePath,'main.css') // 如果不在index.js 里面引入，就要在这里指定路径
-    }
-    cfg.out = {
-        path: buildPath,//存放文件的绝对路径
+    };
+    cfg.output = {
+        path: __dirname + '/build',//存放文件的绝对路径
         buildPath: isBuild ? '' : 'http://192.168.18.240:7000/',//网站运行时的访问路径
-        filename: isBuild ? 'js/[name].[chunkhash:8].js' : '[name].bundle.js'
-    }
+        filename: isBuild ? 'js/[name].[chunkhash:8].js' : '[name].bundle.js',
+        chunkFilename: isBuild ? 'js/[name].[chunkhash:8].js' : '[name].bundle.js'
+    };
 
     if (isBuild) {
         cfg.devtool = 'source-map';
     } else {
         cfg.devtool = 'eval-source-map';
     }
-
-
+    cfg.resolve={alias: {} };
     cfg.module = {
+        noParse:[],
         loaders:[
             {
                 test: /\.js$/,
@@ -41,26 +55,80 @@ module.exports = function cfg (){
                 },
                 exclude: /node_modules/
             },
-            { 
-                test: /\.css$/, 
-                loader:ExtractTextPlugin.extract('style', 'css')
-            },
-            { test: /\.png$/, loader: "url-loader?limit=100000" },
-            { test: /\.jpg$/, loader: "file-loader" }
+            { test: /\.png$/, loader: "url-loader?limit=300000" }
         ]
     };    
-
+    
     cfg.plugins = [
-        new HtmlWebpackPlugin({
-            template:'dev/index.html',
-            inject:'body'
+        new webpack.DefinePlugin({
+           __DEV__: isDev //就可以在js代码中用if(__DEV__){...}判断了
         }),
-        new ExtractTextPlugin('[name].[chunkhash].css')
-    ]
+        new webpack.BannerPlugin("Copyright qiaoxm")
+    ];
 
+    if (!isBuild) {
+        cfg.plugins.push(
+            new HtmlWebpackPlugin({
+                template: 'dev/index.html',
+                inject: 'body'
+            })
+        );
+        cfg.module.loaders = cfg.module.loaders.concat({
+            test: /\.css$/,
+            loaders: ['style', 'css','postcss']
+        });
+    }
+
+    if(isBuild){
+        cfg.module.loaders = cfg.module.loaders.concat({
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+        });
+
+        cfg.plugins.push(
+            new HtmlWebpackPlugin({
+                template: 'dev/index.html',
+                inject: 'body',
+                minify: {
+                    removeComments: true,
+                    minifyJS: true,
+                    collapseWhitespace: true
+                }
+            }),
+            new webpack.NoErrorsPlugin(),
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                },
+                output: {
+                    comments: false
+                }
+            }),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new ExtractTextPlugin('css/[name].[contenthash:8].css')
+            // ,
+            // new CopyWebpackPlugin([
+            //     {
+            //         from: __dirname + 'dev'
+            //     }
+            // ])            
+        );
+    }
+    cfg.postcss = [ autoprefixer({ browsers: ['last 3 versions', '> 1%'] }) ];
     cfg.devServer = {
         contentBase: 'dev',
         stats: 'minimal'
     };
+    deps.forEach(function (dep) { //当引入更多的库时，这样写可以减少冗余
+        var depPath = path.resolve(node_modules_dir, dep);
+        cfg.resolve.alias[dep.split(path.sep)[0]]= depPath;
+        //path.sep 特定平台的文件分隔工具. '\\' 或者 '/'.
+        cfg.module.noParse.push(depPath); //告诉当webpack尝试去解析压缩文件时，这种行为是不允许的
+    });
+
     return cfg;
+
 }();
+
+// rm -rf 要安装rimraf 
